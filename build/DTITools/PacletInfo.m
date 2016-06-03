d2829c59b3c2cd0ccf461dc148f44c2db480183d
Martijn Froeling <m.froeling@gmail.com>
2016-06-03 12:12:47 +0200

    build files

---
+++ build/DTITools/PacletInfo.m
@@ -0,0 +1,15 @@
+(* Paclet Info File *)
+
+(* created 2016/03/23*)
+
+Paclet[
+    Name -> "DTITools",
+    Version -> "1.3.0",
+    MathematicaVersion -> "9+",
+    Extensions -> 
+        {
+            {"Documentation", Language -> "English", MainPage -> "Guides/DTITools"}
+        }
+]
+
+
