2a8b34eef364a888797b52ec2f9d5c306a376cb8
Martijn Froeling <m.froeling@gmail.com>
2019-03-05 10:42:41 +0100

    windows and mac path bug fixes

--- PacletInfo.m
+++ PacletInfo.m
@@ -1,12 +1,12 @@
 (* Paclet Info File *)
 
-(* created 2019/02/26*)
+(* created 2019/03/01*)
 
 Paclet[
     Name -> "QMRITools",
     Version -> "2.1.0",
     MathematicaVersion -> "11.0+",
-    Description -> "Toolbox for Qunatitative MRI.",
+    Description -> "Toolbox for Quantitative MRI.",
     Creator -> "Martijn Froeling <m.froeling@gmail.com>",
     Thumbnail -> "zthumb.gif",
     Extensions -> 
