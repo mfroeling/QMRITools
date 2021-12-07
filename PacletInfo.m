ecd7226d6d1806c8754ca2a6c7f0c6d5d3bfcbc0
Martijn Froeling <m.froeling@gmail.com>
2021-12-07 11:24:22 +0100

    update version for release

--- PacletInfo.m
+++ PacletInfo.m
@@ -1,10 +1,10 @@
 (* Paclet Info File *)
 
-(* created 2021/11/09*)
+(* created 2021/12/06*)
 
 Paclet[
     Name -> "QMRITools",
-    Version -> "2.6.1",
+    Version -> "2.6.3",
     WolframVersion -> "12.3+",
     Description -> "Toolbox for Quantitative MRI.",
     Creator -> "Martijn Froeling <m.froeling@gmail.com>",
